c5fb42fef29807888370b2a190bfb1e58043cbe6
Lauren Small <lauren.small@detroitlabs.com>
2019-02-26 14:13:39 -0500

    Adding a README file and new Xcode project

---
+++ WeatherAppMVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherAppMVC
+//
+//  Created by Lauren Small on 2/26/19.
+//  Copyright © 2019 Lauren Small. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
