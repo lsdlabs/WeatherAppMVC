7d3ed50670205941d643da023bf614fd7293bf93
Lauren Small <lauren.small@detroitlabs.com>
2019-02-26 16:02:54 -0500

    Create current weather data model

---
+++ WeatherAppMVC/Model/CurrentWeather.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentWeather.swift
+//  WeatherAppMVC
+//
+//  Created by Lauren Small on 2/26/19.
+//  Copyright © 2019 Lauren Small. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeather {
+    
+    let temperature: Double
+    let humidity: Double
+    let precipProbability: Double
+    let summary: String
+
+}
